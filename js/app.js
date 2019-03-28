685c9a6ae4b3a83cf5656ff097ecb0e7dd823910
Juan Plaza <plazabjc@gmail.com>
2019-03-27 19:25:30 -0500

    cambio de generar dulces y agregado el encontrar aciertos

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
-//titulo del juego
+
 $(document).ready(function()
 {
   setTimeout(animateTitle,4000);
-  generarDulces()
+  generarTablero();
+  pintarTablero();
+  encontrarAciertos();
+  console.log(tablero);
+  console.log(encontrarAciertos())
 });
 
+// Animar titulo
 function animateTitle()
 {
   var div = $('h1');
@@ -29,28 +34,93 @@ function animateTitle()
 }
 
 // generar dulces aleatorimente 
-var DULCES_POR_COL = 6;
-function generarSrc(){
-    var numero = Math.floor((Math.random()*(4 - 1)) + 1);
-    var ruta = "image/"+numero+".png";
-    return ruta;
+var DULCES_POR_COL = 7;
+var tablero = [];
+
+function genCaramelo() {
+  var numero = Math.floor((Math.random()*(5 - 1)) + 1);
+  return numero;
 }
 
-function generarDulces(){
-
-    for (var i = 0; i < 7; i++) {
-      
-      for (var j = 0 ; j < DULCES_POR_COL; j++) {
-        var imgElement = $("<img>", {
-          'src': generarSrc()
-          })
-        $(".col-"+(i+1)).append(imgElement)
+function generarTablero(){
+  for (var i = 0; i < 7; i++) {
+    tablero[i] = [];
+      for (var j = 0; j < 7; j++) {
+        tablero[i][j] = genCaramelo();
       }
-    }
-}
-
-function generarTimer (tiempo, callback){
-  return function(){
-    setTimeout(callback, tiempo)
   }
 }
+
+function pintarTablero(){
+  for (var i = 0; i < 7; i++) {
+    for (var j = 0 ; j < 7; j++) {
+      var numero = tablero[i][j];
+      var ruta = "image/"+numero+".png";
+      var imgElement = $("<img>", {
+          'src': ruta,
+          'style': 'width: 97px; height: 97px;'
+          })
+      $(".col-"+(i+1)).append(imgElement)
+    }
+  }
+}
+
+//Encontrar aciertos
+
+function encontrarAciertos(){
+  var aciertos = [];
+  for (var i = 0; i < 7; i++){
+    var contador = 0;
+    for (var j = 0 ; j < 7; j++){
+      if (tablero[i][j] === tablero[i][j-1]) {
+        contador+=1;
+      }
+      else {
+        if (contador >= 2) {
+          aciertos.push({
+            columna: i,
+            posFinal: j - 1,
+            longitud: contador + 1,
+          })
+        }
+        contador = 0;
+      }
+    }
+    if (contador >= 2) {
+      aciertos.push({
+        columna: i,
+        posFinal: 6,
+        longitud: contador + 1,
+      })
+    }
+  }
+  for (var j = 0; j < 7; j++){
+    var contador = 0;
+    for (var i = 1 ; i < 7; i++){
+      if (tablero[i][j] === tablero[i-1][j]) {
+        contador+=1;
+      }
+      else {
+        if (contador >= 2) {
+          aciertos.push({
+            fila: j,
+            posFinal: i - 1,
+            longitud: contador + 1,
+          })
+        }
+        contador = 0;
+      }
+    }
+    if (contador >= 2) {
+      aciertos.push({
+        fila: j,
+        posFinal: 6,
+        longitud: contador + 1,
+      })
+    }
+  }
+  return aciertos;
+}
+
+
+
